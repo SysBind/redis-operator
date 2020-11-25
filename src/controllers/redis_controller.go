aa596c017643296f3672629e333f41ca7f2a5c2f
Asaf Ohayon <asaf@sysbind.co.il>
2020-11-25 15:29:18 +0200

    continued with statefulset creation

--- src/controllers/redis_controller.go
+++ src/controllers/redis_controller.go
@@ -18,13 +18,19 @@ package controllers
 
 import (
 	"context"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/go-logr/logr"
+	redisv1 "gitlab.sysbind.biz/SRE/redis-operator/api/v1"
+	kapps "k8s.io/api/apps/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+)
 
-	redisv1 "gitlab.sysbind.biz/SRE/redis-operator/api/v1"
+var (
+	stsOwnerKey = ".metadata.controller"
+	apiGVStr    = redisv1.GroupVersion.String()
 )
 
 // RedisReconciler reconciles a Redis object
@@ -36,11 +42,32 @@ type RedisReconciler struct {
 
 // +kubebuilder:rbac:groups=redis.sysbind.co.il,resources=redis,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=redis.sysbind.co.il,resources=redis/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=apps,resources=statefulsets/status,verbs=get
 
 func (r *RedisReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("redis", req.NamespacedName)
+
+	constructStatefulSetForRedis := func(redis *redisv1.Redis) (*kapps.StatefulSet, error) {
+		spec := kapps.StatefulSetSpec{}
+		//spec.Selector = metav1.LabelSelector{}
+		sts := &kapps.StatefulSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels:      make(map[string]string),
+				Annotations: make(map[string]string),
+				Name:        redis.Name,
+				Namespace:   redis.Namespace,
+			},
+			Spec: spec,
+		}
+
+		if err := ctrl.SetControllerReference(redis, sts, r.Scheme); err != nil {
+			return nil, err
+		}
+
+		return sts, nil
+	}
 
 	var redis redisv1.Redis
 	if err := r.Get(ctx, req.NamespacedName, &redis); err != nil {
@@ -53,16 +80,55 @@ func (r *RedisReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 
 	log.Info("Reconciling ", "redis", req.NamespacedName.Name)
 	log.Info("Structure", "masters", redis.Spec.Masters, "replicas", redis.Spec.Replicas)
-	if redis.Spec.Replicas == nil || redis.Spec.Masters == nil {
-		log.Info("Still no spec, re-queueing..")
-		return ctrl.Result{Requeue: true}, nil
+
+	var childSTS kapps.StatefulSetList
+	if err := r.List(ctx, &childSTS, client.InNamespace(req.Namespace), client.MatchingFields{stsOwnerKey: req.Name}); err != nil {
+		log.Error(err, "unable to list child StatefulSets")
+		return ctrl.Result{}, err
 	}
+
+	for i, sts := range childSTS.Items {
+		log.Info("found statefulset", "idx", i, "sts", sts)
+	}
+
+	if newsts, err := constructStatefulSetForRedis(&redis); err != nil {
+		log.Error(err, "unable to construct statefulset for redis")
+		return ctrl.Result{}, err
+	} else {
+		if err := r.Create(ctx, newsts); err != nil {
+			log.Error(err, "unable to create Statefulset for Redis", "statefuleset", newsts)
+			return ctrl.Result{}, err
+		}
+		log.Info("Created Statefulset for Redis")
+	}
+
+	// r.Create()
+
 	return ctrl.Result{}, nil
 }
 
 func (r *RedisReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Setup stsOwnerKey Index for better searching statefulsets owned by this controller.
+	if err := mgr.GetFieldIndexer().IndexField(&kapps.StatefulSet{}, stsOwnerKey, func(rawObj runtime.Object) []string {
+		// grab the job object, extract the owner...
+		sts := rawObj.(*kapps.StatefulSet)
+		owner := metav1.GetControllerOf(sts)
+		if owner == nil {
+			return nil
+		}
+		// ...make sure it's a CronJob...
+		if owner.APIVersion != apiGVStr || owner.Kind != "Redis" {
+			return nil
+		}
+
+		// ...and if so, return it
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&redisv1.Redis{}).
-		Owns()
+		Owns(&kapps.StatefulSet{}).
 		Complete(r)
 }
